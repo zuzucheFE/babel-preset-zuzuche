bc0779315ba528d0cfad96bb144b4a64475e2f17
kidneyleung <kidneyleung@gmail.com>
2017-12-26 17:38:24 +0800

    初始化项目

---
+++ index.js
@@ -0,0 +1,51 @@
+const extend = require('extend');
+
+function isType(s, typeString) {
+    return {}.toString.call(s) === `[object ${typeString}]`;
+}
+
+function isObject(s) {
+    return isType(s, 'Object');
+}
+
+
+const DEFAULT_ENV_OPTIONS = {
+    'targets': {
+        chrome: 45,
+        firefox: 52,
+        safari: 8,
+        ios: 9,
+        android: 4,
+        ie: 9,
+        edge: 12
+    },
+    'useBuiltIns': false,
+    'modules': false,
+    'debug': false
+};
+function generateENVOptions(options) {
+    return extend(true, {}, DEFAULT_ENV_OPTIONS, options.additionalTargets);
+}
+
+module.export = function (context, options) {
+    let envOptions = (options && isObject(options.env)) ? options.env :
+        generateENVOptions(options.env);
+
+    return {
+        cacheDirectory: options.cacheDirectory || true,
+        presets: [
+            [require.resolve('@babel/preset-env'), envOptions],
+            require.resolve('@babel/preset-react'),
+            require.resolve('@babel/preset-stage-1')
+        ],
+        plugins: [
+            require.resolve('@babel/plugin-syntax-dynamic-import'), // Allow parsing of import()
+            [require.resolve('@babel/plugin-transform-runtime'), {
+                'helpers': false,
+                'polyfill': true,
+                'regenerator': true
+            }],
+            require.resolve('babel-plugin-transform-decorators-legacy')
+        ]
+    }
+};
