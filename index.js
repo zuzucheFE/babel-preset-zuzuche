8cfd145d53a2a87500320bad549ccf40c5e2c489
kidneyleung <kidneyleung@gmail.com>
2018-09-29 18:30:00 +0800

    升级@babel组件，修改transform-runtime和env的默认配置

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var path = require('path');
 var obj = {};
@@ -10,50 +10,87 @@ function isObject(s) {
     return isType(s, 'Object');
 }
 
+function validateBoolOption(name, value, defaultValue) {
+    if (typeof value === 'undefined') {
+        value = defaultValue;
+    }
+
+    if (typeof value !== 'boolean') {
+        throw new Error('Preset zuzuche: "' + name + '" option must be a boolean.');
+    }
+
+    return value;
+}
+
 var assign = Object.assign;
 
 var DEFAULT_ENV_OPTIONS = {
-    'targets': {
+    targets: {
         browsers: [
             'Chrome >= 45', 'last 2 Firefox versions',
             'ie >= 9', 'Edge >= 12',
             'iOS >= 9', 'Android >= 4', 'last 2 ChromeAndroid versions'
         ]
     },
-    'useBuiltIns': false,
-    'modules': false,
-    'debug': false
+    ignoreBrowserslistConfig: true,
+    useBuiltIns: false,
+    modules: false,
+    debug: false
 };
 
 var DEFAULT_TRANSFORM_RUNTIME_OPTIONS = {
-    'helpers': false,
-    'polyfill': true,
-    'regenerator': true,
-    'moduleName': path.dirname(require.resolve('@babel/runtime/package.json'))
+    corejs: false,
+    helpers: true,
+    regenerator: true,
+    absoluteRuntime: true
 };
 
 module.exports = function (context, options) {
     var ENV = process.env.BABEL_ENV || process.env.NODE_ENV;
     var isEnvDevelopment = ENV === 'development';
 
+    // ====================
+    // presets config
     var envOptions = (options && isObject(options.env)) ?
         assign({}, DEFAULT_ENV_OPTIONS, options.env) :
         assign({}, DEFAULT_ENV_OPTIONS);
 
+    var presets = [
+        [require('@babel/preset-env').default, envOptions],
+        [require('@babel/preset-react').default, {
+            development: isEnvDevelopment
+        }]
+    ];
+
+    var isFlowEnabled = validateBoolOption('flow', options.flow, true);
+    if (isFlowEnabled) {
+        presets.push(require('@babel/preset-flow').default);
+    }
+
+
+    // ====================
+    // plugins config
     var transformRuntimeOptions = (options && isObject(options.transformRuntime)) ?
         assign({}, DEFAULT_TRANSFORM_RUNTIME_OPTIONS, options.transformRuntime) :
         assign({}, DEFAULT_TRANSFORM_RUNTIME_OPTIONS);
 
+    if (
+        typeof transformRuntimeOptions.useAbsoluteRuntime === 'boolean' &&
+        transformRuntimeOptions.useAbsoluteRuntime === true
+    ) {
+        transformRuntimeOptions.useAbsoluteRuntime = path.dirname(
+            require.resolve('@babel/runtime/package.json')
+        );
+    }
+
+    var plugins = [
+        // Adds syntax support for import()
+        require('@babel/plugin-syntax-dynamic-import').default,
+        [require('@babel/plugin-transform-runtime').default, transformRuntimeOptions]
+    ];
+
     return {
-        presets: [
-            [require.resolve('@babel/preset-env'), envOptions],
-            require.resolve('@babel/preset-react', {
-                development: isEnvDevelopment
-            })
-        ],
-        plugins: [
-            require.resolve('@babel/plugin-syntax-dynamic-import'), // Allow parsing of import()
-            [require.resolve('@babel/plugin-transform-runtime'), transformRuntimeOptions]
-        ]
+        presets: presets,
+        plugins: plugins
     };
 };
